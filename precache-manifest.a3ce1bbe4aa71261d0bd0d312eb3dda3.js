d24dd6d9166c056bf5d5c626660a7dfc840ad1ae
Vlad <vladyslav.kulyk@gmail.com>
2020-07-23 18:56:43 +0300

    Updates

---
+++ precache-manifest.a3ce1bbe4aa71261d0bd0d312eb3dda3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f49a8d17684763203c6cd9808596224f",
+    "url": "/game-adventure-capitalist/index.html"
+  },
+  {
+    "revision": "fb6926e82c3e46e4ce1b",
+    "url": "/game-adventure-capitalist/static/js/2.389967af.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/game-adventure-capitalist/static/js/2.389967af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73eff85137061aafaea4",
+    "url": "/game-adventure-capitalist/static/js/main.abb5e006.chunk.js"
+  },
+  {
+    "revision": "277cf7b9f39b626fc7f5",
+    "url": "/game-adventure-capitalist/static/js/runtime-main.c91eb6dc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-game-adventure-capitalist/precache-manifest.433982c045eea934cfda8dcae9ac6f4e.js"
+  "/game-adventure-capitalist/precache-manifest.a3ce1bbe4aa71261d0bd0d312eb3dda3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-game-adventure-capitalist/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game-adventure-capitalist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
